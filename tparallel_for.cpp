edf6253f9b9cfb313dbe695cedc70f44398b233e
hungdang <hungptit@gmail.com>
2016-08-19 17:11:51 -0400

    Add tdd:parallel_for and tbb:concurrent_vector examples.

--- tparallel_for.cpp
+++ tparallel_for.cpp
@@ -4,19 +4,47 @@
 
 #include "utils/Timer.hpp"
 
-int main() {
-    const int size = 20000000;
-    std::vector<double> output(size, 0);
-    std::vector<double> input(size, 0);
+template <typename Container> void print(Container &&data, const std::string &msg) {
+    if (data.size() > 20) {
+        return;
+    }
+    std::cout << msg << "\n";
+    std::for_each(data.begin(), data.end(), [](auto const &item) { std::cout << item << " "; });
+    std::cout << "\n";
+}
+
+template <typename Container> void test_parallel_for(const int size){
+    Container output(size);
+    Container input(size);
 
     for (int i = 0; i < size; i++) {
         input[i] = i;
     }
 
-    utils::ElapsedTime<utils::MILLISECOND> timer("Time: ");
-    tbb::parallel_for(0, size, 1000, [&output, &input](int i) {
-        output[i] = sqrt(sin(input[i]) * sin(input[i]) + cos(input[i]) * cos(input[i]));
-    });
+    {
+        utils::ElapsedTime<utils::MILLISECOND> timer("Parallel for: ");
+        tbb::parallel_for(0, size, 1, [&output, &input](int i) {
+            output[i] = sqrt(sin(input[i]) * sin(input[i]) + cos(input[i]) * cos(input[i]));
+        });
+        print(output, "parallel_for");
+    }
+
+    {
+        utils::ElapsedTime<utils::MILLISECOND> timer("Simple for loop: ");
+        const size_t N = size;
+        for (size_t idx = 0; idx < N; ++idx) {
+            output[idx] =
+                sqrt(sin(input[idx]) * sin(input[idx]) + cos(input[idx]) * cos(input[idx]));
+        }
+        print(output, "simple for");
+    }
+}
+
+
+int main() {
+    const int size = 20000000;
+    test_parallel_for<std::vector<double>>(size);
+    test_parallel_for<tbb::concurrent_vector<double>>(size);
 
     return 0;
 }
