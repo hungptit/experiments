4f9b0ccae531d3820aaefa75b3658f2ab49d8532
hungdang <hungptit@gmail.com>
2016-08-19 16:50:19 -0400

    Add parallel_for example.

--- tparallel_for.cpp
+++ tparallel_for.cpp
@@ -2,15 +2,19 @@
 #include <cmath>
 #include <vector>
 
+#include "utils/Timer.hpp"
+
 int main() {
     const int size = 20000000;
-    std::vector<double> output(size, 0), input(size, 0);
+    std::vector<double> output(size, 0);
+    std::vector<double> input(size, 0);
 
     for (int i = 0; i < size; i++) {
         input[i] = i;
     }
 
-    tbb::parallel_for(0, size, 1, [=](int i) {
+    utils::ElapsedTime<utils::MILLISECOND> timer("Time: ");
+    tbb::parallel_for(0, size, 1000, [&output, &input](int i) {
         output[i] = sqrt(sin(input[i]) * sin(input[i]) + cos(input[i]) * cos(input[i]));
     });
 
