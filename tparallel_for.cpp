7919dbc3d0a8d1001e75c74f316005b8ec515c33
hungdang <hungptit@gmail.com>
2016-08-20 23:16:31 -0400

    Add tbb example.

--- tparallel_for.cpp
+++ tparallel_for.cpp
@@ -13,18 +13,21 @@ template <typename Container> void print(Container &&data, const std::string &ms
     std::cout << "\n";
 }
 
-template <typename Container> void test_parallel_for(const int size){
-    Container output(size);
+template <typename Container> void test_parallel_for(const int size) {
+    Container output;
     Container input(size);
 
     for (int i = 0; i < size; i++) {
         input[i] = i;
     }
+    output.reserve(size);
 
+    
     {
         utils::ElapsedTime<utils::MILLISECOND> timer("Parallel for: ");
         tbb::parallel_for(0, size, 1, [&output, &input](int i) {
-            output[i] = sqrt(sin(input[i]) * sin(input[i]) + cos(input[i]) * cos(input[i]));
+            output.push_back(
+                sqrt(sin(input[i]) * sin(input[i]) + cos(input[i]) * cos(input[i])));
         });
         print(output, "parallel_for");
     }
@@ -33,17 +36,16 @@ template <typename Container> void test_parallel_for(const int size){
         utils::ElapsedTime<utils::MILLISECOND> timer("Simple for loop: ");
         const size_t N = size;
         for (size_t idx = 0; idx < N; ++idx) {
-            output[idx] =
-                sqrt(sin(input[idx]) * sin(input[idx]) + cos(input[idx]) * cos(input[idx]));
+            output.push_back(
+                sqrt(sin(input[idx]) * sin(input[idx]) + cos(input[idx]) * cos(input[idx])));
         }
         print(output, "simple for");
     }
 }
 
-
 int main() {
     const int size = 20000000;
-    test_parallel_for<std::vector<double>>(size);
+    // test_parallel_for<std::vector<double>>(size);
     test_parallel_for<tbb::concurrent_vector<double>>(size);
 
     return 0;
