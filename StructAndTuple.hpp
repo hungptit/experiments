b05269b5134049ab6cbda35e6da5a7030fef618d
hungdang <hungptit@gmail.com>
2016-08-24 14:53:47 -0400

    Update the performance test.

--- StructAndTuple.hpp
+++ StructAndTuple.hpp
@@ -12,11 +12,16 @@
 
 #include "Timer.hpp"
 
+using first_type = char;
+using second_type = int;
+using third_type = char;
+using fourth_type = int;
+
 struct Data {
-    Data() : first(), third(), second(), fourth() {}
+    Data() : first(), second(), third(), fourth() {}
 
     template <typename T1, typename T2, typename T3, typename T4>
-    Data(T1 &&v1, T2 &&v2, T3 &&v3, T4 &&v4) : first(v1), third(v3), second(v2), fourth(v4) {}
+    Data(T1 &&v1, T2 &&v2, T3 &&v3, T4 &&v4) : first(v1), second(v2), third(v3), fourth(v4) {}
 
     Data(const Data &rhs) {
         first = rhs.first;
@@ -27,7 +32,8 @@ struct Data {
 
     template <typename T>
     Data(T &&rhs)
-        : first(rhs.first), third(rhs.third), second(rhs.second), fourth(rhs.fourth) {}
+        : first(std::move(rhs.first)), second(std::move(rhs.second)),
+          third(std::move(rhs.third)), fourth(std::move(rhs.fourth)) {}
 
     Data &operator=(const Data &rhs) {
         if (this == &rhs)
@@ -39,34 +45,30 @@ struct Data {
         return *this;
     }
 
-    int first;
-    int third;
-    char second;
-    char fourth;
+    first_type first;
+    second_type second;
+    third_type third;
+    fourth_type fourth;
 };
 
-// clang-format off
 bool operator<(const Data &lhs, const Data &rhs) {
-    return (lhs.first < rhs.first) ||
-        ((lhs.first == rhs.first) && (lhs.second < rhs.second)) ||
-        ((lhs.first == rhs.first) && (lhs.second == rhs.second) && (lhs.third < rhs.third)) ||
-        ((lhs.first == rhs.first) && (lhs.second == rhs.second) && (lhs.third == rhs.third) && (lhs.fourth < rhs.fourth));    
+    return std::tie(lhs.first, lhs.second, lhs.third, lhs.fourth) <
+           std::tie(rhs.first, rhs.second, rhs.third, rhs.fourth);
 }
-// clang-format on
 
 bool operator==(const Data &lhs, const Data &rhs) {
     return (lhs.first == rhs.first) && (lhs.second == rhs.second) && (lhs.third == rhs.third) &&
            (lhs.fourth == rhs.fourth);
 }
 
-using TupleData = std::tuple<int, int, char, char>;
+using TupleData = std::tuple<first_type, second_type, third_type, fourth_type>;
 
 template <typename Container> Container test_data(const size_t N) {
     Container data(N);
     std::mt19937 rng;
     rng.seed(std::random_device()());
     std::uniform_int_distribution<std::mt19937::result_type> charDist(
-        1, std::numeric_limits<char>::max());
+        1, std::numeric_limits<int>::max());
     std::uniform_int_distribution<std::mt19937::result_type> intDist(
         1, std::numeric_limits<int>::max());
     using value_type = typename Container::value_type;
@@ -81,7 +83,7 @@ template <typename T1, typename T2> T1 create_tuple_data(T2 &&data) {
     T1 results(N);
     for (size_t idx = 0; idx < N; ++idx) {
         auto const item = data[idx];
-        results[idx] = std::make_tuple(item.first, item.third, item.second, item.fourth);
+        results[idx] = std::make_tuple(item.first, item.second, item.third, item.fourth);
     }
     return results;
 }
@@ -89,6 +91,26 @@ template <typename T1, typename T2> T1 create_tuple_data(T2 &&data) {
 template <typename Container> void test_sort(Container &data, const std::string &msg) {
     utils::ElapsedTime<utils::MILLISECOND> t(msg);
     std::sort(data.begin(), data.end());
+}
+
+template <typename Container> size_t test_sum_struct(Container &data, const std::string &msg) {
+    utils::ElapsedTime<utils::MILLISECOND> t(msg);
+    using value_type = typename Container::value_type;
+    size_t sum = 0;
+    std::for_each(data.begin(), data.end(), [&sum](const value_type &item) {
+        sum += item.first + item.second + item.third + item.fourth;
+    });
+    return sum;
+}
+
+template <typename Container> size_t test_sum_tuple(Container &data, const std::string &msg) {
+    utils::ElapsedTime<utils::MILLISECOND> t(msg);
+    using value_type = typename Container::value_type;
+    size_t sum = 0;
+    std::for_each(data.begin(), data.end(), [&sum](const value_type &item) {
+        sum += std::get<0>(item) + std::get<1>(item) + std::get<2>(item) + std::get<3>(item);
+    });
+    return sum;
 }
 
 template <typename Container> void test_tbb_sort(Container &data, const std::string &msg) {
--- tTuplevsStruct.cpp
+++ tTuplevsStruct.cpp
@@ -3,22 +3,31 @@
 void test_sum() {}
 
 int main() {
-    size_t N = 20000000;
+    size_t N = 200000000;
     using StructContainer = std::vector<Data>;
     using TupleContainer = std::vector<TupleData>;
 
     constexpr int LOOP = 5;
+
+    // for (auto idx = 1; idx < LOOP; ++idx) {
+    //     StructContainer sdata = test_data<StructContainer>(N);
+    //     TupleContainer tdata = create_tuple_data<TupleContainer>(sdata);
+    //     test_sort(tdata, "Sort for tuple: ");
+    //     test_sort(sdata, "Sort for struct: ");
+    // }
+
+
     for (auto idx = 1; idx < LOOP; ++idx) {
         StructContainer sdata = test_data<StructContainer>(N);
         TupleContainer tdata = create_tuple_data<TupleContainer>(sdata);
-        test_sort(sdata, "Sort for struct: ");
-        test_sort(tdata, "Sort for tuple: ");
+        std::cout << test_sum_tuple(tdata, "Sum for tuple: ") << "\n";
+        std::cout << test_sum_struct(sdata, "Sum for struct: ") << "\n";;
     }
 
-    // {
-    //     StructContainer sdata = test_data<StructContainer>(N);
-    //     TupleContainer tdata = create_tuple_data<TupleContainer>(sdata);
-    //     test_tbb_sort(sdata, "Sort for struct: ");
-    //     test_tbb_sort(tdata, "Sort for tuple: ");
-    // }
+    for (auto idx = 1; idx < LOOP; ++idx) {
+        StructContainer sdata = test_data<StructContainer>(N);
+        TupleContainer tdata = create_tuple_data<TupleContainer>(sdata);
+        std::cout << test_sum_struct(sdata, "Sum for struct: ") << "\n";;
+        std::cout << test_sum_tuple(tdata, "Sum for tuple: ") << "\n";
+    }
 }
