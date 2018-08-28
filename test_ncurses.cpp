64b7a49d1d8da5100d84f5d66361802c0429d619
Hung Dang [hdang] <hdang@athenahealth.com>
2018-08-28 13:49:59 -0400

    Add an example for ncurses.

--- test_ncurses.cpp
+++ test_ncurses.cpp
@@ -1,16 +1,36 @@
 #include <ncurses.h>
 #include <string>
 
+namespace console {
+    constexpr char BACKSPACE = 127;
+    constexpr char CTRL_D = 4;
+    constexpr char ENTER = 10;
+} // namespace console
+
 int main() {
     initscr(); /* Start curses mode 		  */
     refresh(); /* Print it on to the real screen */
     std::string buffer;
-    while (1) {
+    bool isok = true;
+    while (isok) {
+        noecho();				// Do not echo.
         char key = getch();
-        if (key == 4)
+        mvprintw(1, 1, "Enter: %d\n", key);
+        switch (key) {
+        case console::BACKSPACE:
+            if (!buffer.empty()) {
+                buffer.pop_back();
+            }
             break;
-        buffer.push_back(key);
-        printw("%s", buffer.data()); /* Print Hello World		  */
+        case console::ENTER:
+            break;
+        case console::CTRL_D:
+            isok = false;
+            break;
+        default:
+            buffer.push_back(key);
+        }
+		mvprintw(2, 1, "%s", buffer.data()); /* Print Hello World		  */
     }
     endwin(); /* End curses mode		  */
 
